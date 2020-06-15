2c8e049e46d14062c77c59295489cf4f6579228b
gostjoke <gostjoketw@gmail.com>
2020-06-14 22:50:57 -0700

    lab3.2

--- test.py
+++ test.py
@@ -1,17 +1,23 @@
-#(c.5)
-def calculate_shipping(weight:int) -> int:
-    "under 2 pounds, the rate is $2.00."
-    if weight < 2:
-        return 2
-    "2 pounds but under 10 pounds, the rate is $5.00."
-    if 2 <= weight < 10:
-        return 5
-    "packages of 10 pounds or more, the rate is $5.00 plus $1.50 per pound for each pound over 10."
-    if weight >= 10:
-        return 5 + (weight-10) * 1.5
+#c.6
+import tkinter              # load the library; do this just once per program
 
-assert calculate_shipping(1.5) == 2.00
-assert calculate_shipping(7) == 5.00
-assert calculate_shipping(15) == 12.50
+my_window = tkinker.Tk()    # Create the graphics window
 
-print()
+my_canvas = tkinter.Canvas(my_window, width=500, height=500) # Create a 500x500 canvas to draw on
+my_canvas.pack()            # Put the canvas into the window
+
+#x-values extend positively to the right and y-values extend positively down
+
+#(6)
+def create_square(x1, y1, l:int) -> int: 
+    "take x-coordinate and y-coordinate of the upper-left corner and the length of a side."
+    x2 = x1+ l
+    y2 = y1+ l
+    return my_canvas.create_rectangle(x1,y1,x2,y2)
+
+create_square(100,100,200)
+
+
+
+tkinker.mainloop()           #combine all the elements and diplay the window
+
--- test2.py
+++ test2.py
@@ -1,1 +1,29 @@
+import tkinter              # Load the library; do this just once per program
 
+my_window = tkinter.Tk()    # Create the graphics window
+
+my_canvas = tkinter.Canvas(my_window, width=500, height=500)  # Create a 500x500 canvas to draw on
+my_canvas.pack()            # Put the canvas into the window
+
+
+#x-values extend positively to the right and y-values extend positively down
+
+#(6)
+def create_square(x1,y1,l:int) -> int:
+    " takes x-coordinate and y-coordinate of the upper-left corner and the length of a side."
+    x2 = x1+ l
+    y2 = y1+ l
+    return my_canvas.create_rectangle(x1,y1,x2,y2)
+
+create_square(100,100,200)
+
+#(7)
+def create_circle(x1,y1,d:int) -> int:
+    "takes x-coordinate and y-coordinate of the upper-left corner of the square that encloses the circle and the diameter"
+    x2 = x1 + d
+    y2 = y1 + d
+    return my_canvas.create_oval(x1,y1,x2,y2)
+    
+create_circle(100,100,200)
+
+tkinter.mainloop()     
